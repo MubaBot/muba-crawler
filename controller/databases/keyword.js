2cc6064744b21a2971429eb4cf16f9609f88497a
Micalgenus <micalgenus@gmail.com>
2018-08-19 17:39:56 +0900

    Register keyword status code

--- controller/databases/keyword.js
+++ controller/databases/keyword.js
@@ -1,13 +1,14 @@
-const Keyword = require('../../models/keyword');
+const Keyword = require("../../models/keyword");
 
-exports.createKeyword = async (keyword) => {
+exports.createKeyword = async keyword => {
   return await new Keyword({ keyword: keyword }).save().catch(err => err);
-}
+};
 
 exports.getAllKeyword = async () => {
-  const keywords = Keyword.find({}).sort('updatedAt');
+  const keywords = Keyword.find({}, "keyword updatedAt").sort("updatedAt");
   return keywords;
-}
+};
+
 exports.getKeywordByName = async keyword => {
   return Keyword.findOne({ keyword: keyword });
 };
--- controller/keyword/index.js
+++ controller/keyword/index.js
@@ -2,17 +2,24 @@ const config = require("../../config");
 const works = require("../databases/works");
 const keyword = require("../databases/keyword");
 
-exports.create = (req, res, next) => {
-  (async () => {
-    const k = req.body.keyword;
-    // if exist keyword
-    // return error
+exports.create = async (req, res, next) => {
+  const k = req.body.keyword;
+  if (!k) return res.status(412).json({ success: -1 });
 
-    for (let engine in config.engines) await works.createWork(engine, k, config.engines[engine]);
-    await keyword.createKeyword(k);
+  const exist = await keyword.getKeywordByName(k);
+  if (exist) return res.status(409).json({ success: -2 });
 
-    return res.send({ keyword: k });
-  })();
+  for (let engine in config.engines) await works.createWork(engine, k, config.engines[engine]);
+  await keyword.createKeyword(k);
+
+  return res.send({ success: 0 });
+};
+
+exports.reWorking = async (req, res, next) => {
+  const k = req.body.keyword;
+  for (let engine in config.engines) await works.createWork(engine, k, config.engines[engine]);
+
+  return res.send({ success: 0 });
 };
 
 exports.allKeyword = (req, res, next) => {
