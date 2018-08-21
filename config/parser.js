fd205f612a9fecd2b2954c313eca6b3ff77871c5
Micalgenus <micalgenus@gmail.com>
2018-08-21 12:41:47 +0900

    Modify naver blog parser tag

--- config/parser.js
+++ config/parser.js
@@ -8,7 +8,7 @@ const parser = {
   'm.blog.naver.com': {
     path: 'naver/blog',
     title: '.se_title',
-    content: '.se_component_wrap ',
+    content: '.__se_component_area ',
     comments: 'comments'
   },
   '*.tistory.com': {
