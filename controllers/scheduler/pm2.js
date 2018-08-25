45cf14584d6d95eaae5096db0e7c785206cb41d4
Micalgenus <micalgenus@gmail.com>
2018-08-25 23:13:08 +0900

    Change parsing time interval
    10s -> 3s

--- controllers/scheduler/pm2.js
+++ controllers/scheduler/pm2.js
@@ -21,7 +21,7 @@ new Promise(resolve =>
     length = c.length;
 
     setInterval(getList, 15000);
-    setInterval(getUrlContent, 10000 / length);
+    setInterval(getUrlContent, 3000 / length);
   })
   .catch(err => console.log(err));
 
